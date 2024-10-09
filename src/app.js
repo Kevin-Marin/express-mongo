002c964b4ec040e83dab9cea437f3def3a6ce2d4
Kevin Marin <kuglermarinkevin@gmail.com>
2024-10-09 19:53:38 -0300

    feat: delete

--- src/app.js
+++ src/app.js
@@ -5,38 +5,20 @@ import routes from "./routes/index.js";
 const conexao = await conectaNaDatabase();
 
 conexao.on("error", (erro) => {
-    console.error("erro de conexão", erro);
+  console.error("erro de conexão", erro);
 });
 
 conexao.once("open", () => {
-    console.log("Conexão com o banco feita com sucesso!")
-});
+  console.log("Conexao com o banco feita com sucesso");
+})
 
 const app = express();
-routes(app)
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("livro cadastrado com sucesso");
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(200).json(livros);
-});
+routes(app);
 
 app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro removido com sucesso!");
+  const index = buscaLivro(req.params.id);
+  livros.splice(index, 1);
+  res.status(200).send("livro removido com sucesso");
 });
 
 export default app;
-
-
